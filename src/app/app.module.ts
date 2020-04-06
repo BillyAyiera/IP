8b399f47b0126ba84e9e8a259b3f4b7eeacd3297
BillyAyiera <ayierabilly1@gmail.com>
2020-04-06 16:06:15 +0300

    new details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { DateCountPipe } from './date-count.pipe';
     DateCountPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(1, 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live ',
+    'Rick Cook, The Wizardry Compiled', 'Linus Torvalds',
+    new Date(2020, 3, 3), 0, 0),
+    new Quote(2, 'Good programmers write code that humans can understand', 'Martin Fowler', 'Larry Niven',
+    new Date(2020, 3, 3), 0, 0),
+    new Quote(3, 'I\'m not a great programmer; I\'m just a good programmer with great habits..', 'Why The Lucky Stiff', 'Kent Beck',
+    new Date(2020 , 3, 3), 0, 0),
+  ];
+
+  quoteDelete(isRead, index) {
+    if (isRead) {
+      let toDelete: boolean = confirm(`Are you sure you want to delete this Quote?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+
+    }
+  }
+  addNewQuote(quote) {
+  let quoteLength = this.quotes.length;
+  quote.id = quoteLength + 1;
+  quote.completeDate = new Date(quote.completeDate);
+  this.quotes.push(quote);
+  }
+
+  displayInfo(index){
+    this.quotes[index].showInfo = !this.quotes[index].showInfo;
+  }
+
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
+
 
 }
